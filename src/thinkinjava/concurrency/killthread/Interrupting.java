e6bf9660dcf96d407d01f648f461ea2429511048
imckh kompakar <987992518@qq.com>
2019-07-11 18:34:00 +0800

    线程中断/阻塞

---
+++ src/thinkinjava/concurrency/killthread/Interrupting.java
@@ -0,0 +1,83 @@
+package thinkinjava.concurrency.killthread;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+class SleepBlocked implements Runnable {// 可中断的阻塞
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException");
+        }
+        System.out.println("Exiting SleepBlocked.run()");
+    }
+}
+
+class IOBlocked implements Runnable {// 不可中断的阻塞
+    private InputStream in;
+
+    public IOBlocked(InputStream is) {
+        in = is;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Waiting for read():");
+            in.read();
+        } catch (IOException e) {
+            if (Thread.currentThread().isInterrupted()) {
+                System.out.println("Interrupted from blocked I/O");
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Exiting IOBlocked.run()");
+    }
+}
+
+class SynchronizedBlocked implements Runnable {// 不可中断的阻塞
+    public SynchronizedBlocked() {
+        // Lock acquired by this thread
+        new Thread(this::f).start();
+    }
+
+    public synchronized void f() {
+        while (true) // Never releases lock
+            Thread.yield();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Trying to call f()");
+        f();
+        System.out.println("Exiting SynchronizedBlocked.run()");
+    }
+}
+
+public class Interrupting {
+    private static ExecutorService exec = Executors.newCachedThreadPool();
+
+    static void test(Runnable r) throws InterruptedException {
+        Future<?> f = exec.submit(r);
+        TimeUnit.MILLISECONDS.sleep(100);
+        System.out.println("Interrupting " + r.getClass().getName());
+        f.cancel(true); // Interrupts if running
+        System.out.println("Interrupt sent to " + r.getClass().getName());
+    }
+
+    public static void main(String[] args) throws Exception {
+        test(new SleepBlocked());
+        test(new IOBlocked(System.in));
+        test(new SynchronizedBlocked());
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("Aborting with System.exit(0)");
+        System.exit(0); // ... since last 2 interrupts failed
+    }
+}
